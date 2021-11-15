4db254118f4bc998fca6049e89e80ed73f42f997
Tago <42473499+tagoso@users.noreply.github.com>
2021-11-15 21:50:03 +0200

    upload files again

---
+++ play.js
@@ -0,0 +1,739 @@
+const deckStandard = [
+  {
+    name: "SpadesA",
+    suits: "Spade",
+    value: 1,
+    number: "A",
+    color: "Black",
+    face: false,
+    symbol: "♠️",
+    card: "🂡",
+  },
+  {
+    name: "Spades2",
+    suits: "Spade",
+    value: 2,
+    number: "2",
+    color: "Black",
+    face: false,
+    symbol: "♠️",
+    card: "🂢",
+  },
+  {
+    name: "Spades3",
+    suits: "Spade",
+    value: 3,
+    number: "3",
+    color: "Black",
+    face: false,
+    symbol: "♠️",
+    card: "🂣",
+  },
+  {
+    name: "Spades4",
+    suits: "Spade",
+    value: 4,
+    number: "4",
+    color: "Black",
+    face: false,
+    symbol: "♠️",
+    card: "🂤",
+  },
+  {
+    name: "Spades5",
+    suits: "Spade",
+    value: 5,
+    number: "5",
+    color: "Black",
+    face: false,
+    symbol: "♠️",
+    card: "🂥",
+  },
+  {
+    name: "Spades6",
+    suits: "Spade",
+    value: 6,
+    number: "6",
+    color: "Black",
+    face: false,
+    symbol: "♠️",
+    card: "🂦",
+  },
+  {
+    name: "Spades7",
+    suits: "Spade",
+    value: 7,
+    number: "7",
+    color: "Black",
+    face: false,
+    symbol: "♠️",
+    card: "🂧",
+  },
+  {
+    name: "Spades8",
+    suits: "Spade",
+    value: 8,
+    number: "8",
+    color: "Black",
+    face: false,
+    symbol: "♠️",
+    card: "🂨",
+  },
+  {
+    name: "Spades9",
+    suits: "Spade",
+    value: 9,
+    number: "9",
+    color: "Black",
+    face: false,
+    symbol: "♠️",
+    card: "🂩",
+  },
+  {
+    name: "Spades10",
+    suits: "Spade",
+    value: 10,
+    number: "10",
+    color: "Black",
+    face: false,
+    symbol: "♠️",
+    card: "🂪",
+  },
+  {
+    name: "SpadesJ",
+    suits: "Spade",
+    value: 11,
+    number: "J",
+    color: "Black",
+    face: true,
+    symbol: "♠️",
+    card: "🂫",
+  },
+  {
+    name: "SpadesQ",
+    suits: "Spade",
+    value: 12,
+    number: "Q",
+    color: "Black",
+    face: true,
+    symbol: "♠️",
+    card: "🂭",
+  },
+  {
+    name: "SpadesK",
+    suits: "Spade",
+    value: 13,
+    number: "K",
+    color: "Black",
+    face: true,
+    symbol: "♠️",
+    card: "🂮",
+  },
+  {
+    name: "HeartsA",
+    suits: "Heart",
+    value: 1,
+    number: "A",
+    color: "Red",
+    face: false,
+    symbol: "♥️",
+    card: "🂱",
+  },
+  {
+    name: "Hearts2",
+    suits: "Heart",
+    value: 2,
+    number: "2",
+    color: "Red",
+    face: false,
+    symbol: "♥️",
+    card: "🂲",
+  },
+  {
+    name: "Hearts3",
+    suits: "Heart",
+    value: 3,
+    number: "3",
+    color: "Red",
+    face: false,
+    symbol: "♥️",
+    card: "🂳",
+  },
+  {
+    name: "Hearts4",
+    suits: "Heart",
+    value: 4,
+    number: "4",
+    color: "Red",
+    face: false,
+    symbol: "♥️",
+    card: "🂴",
+  },
+  {
+    name: "Hearts5",
+    suits: "Heart",
+    value: 5,
+    number: "5",
+    color: "Red",
+    face: false,
+    symbol: "♥️",
+    card: "🂵",
+  },
+  {
+    name: "Hearts6",
+    suits: "Heart",
+    value: 6,
+    number: "6",
+    color: "Red",
+    face: false,
+    symbol: "♥️",
+    card: "🂶",
+  },
+  {
+    name: "Hearts7",
+    suits: "Heart",
+    value: 7,
+    number: "7",
+    color: "Red",
+    face: false,
+    symbol: "♥️",
+    card: "🂷",
+  },
+  {
+    name: "Hearts8",
+    suits: "Heart",
+    value: 8,
+    number: "8",
+    color: "Red",
+    face: false,
+    symbol: "♥️",
+    card: "🂸",
+  },
+  {
+    name: "Hearts9",
+    suits: "Heart",
+    value: 9,
+    number: "9",
+    color: "Red",
+    face: false,
+    symbol: "♥️",
+    card: "🂹",
+  },
+  {
+    name: "Hearts10",
+    suits: "Heart",
+    value: 10,
+    number: "10",
+    color: "Red",
+    face: false,
+    symbol: "♥️",
+    card: "🂺",
+  },
+  {
+    name: "HeartsJ",
+    suits: "Heart",
+    value: 11,
+    number: "J",
+    color: "Red",
+    face: true,
+    symbol: "♥️",
+    card: "🂻",
+  },
+  {
+    name: "HeartsQ",
+    suits: "Heart",
+    value: 12,
+    number: "Q",
+    color: "Red",
+    face: true,
+    symbol: "♥️",
+    card: "🂽",
+  },
+  {
+    name: "HeartsK",
+    suits: "Heart",
+    value: 13,
+    number: "K",
+    color: "Red",
+    face: true,
+    symbol: "♥️",
+    card: "🂾",
+  },
+  {
+    name: "ClubsA",
+    suits: "Club",
+    value: 1,
+    number: "A",
+    color: "Black",
+    face: false,
+    symbol: "♣️",
+    card: "🃑",
+  },
+  {
+    name: "Clubs2",
+    suits: "Club",
+    value: 2,
+    number: "2",
+    color: "Black",
+    face: false,
+    symbol: "♣️",
+    card: "🃒",
+  },
+  {
+    name: "Clubs3",
+    suits: "Club",
+    value: 3,
+    number: "3",
+    color: "Black",
+    face: false,
+    symbol: "♣️",
+    card: "🃓",
+  },
+  {
+    name: "Clubs4",
+    suits: "Club",
+    value: 4,
+    number: "4",
+    color: "Black",
+    face: false,
+    symbol: "♣️",
+    card: "🃔",
+  },
+  {
+    name: "Clubs5",
+    suits: "Club",
+    value: 5,
+    number: "5",
+    color: "Black",
+    face: false,
+    symbol: "♣️",
+    card: "🃕",
+  },
+  {
+    name: "Clubs6",
+    suits: "Club",
+    value: 6,
+    number: "6",
+    color: "Black",
+    face: false,
+    symbol: "♣️",
+    card: "🃖",
+  },
+  {
+    name: "Clubs7",
+    suits: "Club",
+    value: 7,
+    number: "7",
+    color: "Black",
+    face: false,
+    symbol: "♣️",
+    card: "🃗",
+  },
+  {
+    name: "Clubs8",
+    suits: "Club",
+    value: 8,
+    number: "8",
+    color: "Black",
+    face: false,
+    symbol: "♣️",
+    card: "🃘",
+  },
+  {
+    name: "Clubs9",
+    suits: "Club",
+    value: 9,
+    number: "9",
+    color: "Black",
+    face: false,
+    symbol: "♣️",
+    card: "🃙",
+  },
+  {
+    name: "Clubs10",
+    suits: "Club",
+    value: 10,
+    number: "10",
+    color: "Black",
+    face: false,
+    symbol: "♣️",
+    card: "🃚",
+  },
+  {
+    name: "ClubsJ",
+    suits: "Club",
+    value: 11,
+    number: "J",
+    color: "Black",
+    face: true,
+    symbol: "♣️",
+    card: "🃛",
+  },
+  {
+    name: "ClubsQ",
+    suits: "Club",
+    value: 12,
+    number: "Q",
+    color: "Black",
+    face: true,
+    symbol: "♣️",
+    card: "🃝",
+  },
+  {
+    name: "ClubsK",
+    suits: "Club",
+    value: 13,
+    number: "K",
+    color: "Black",
+    face: true,
+    symbol: "♣️",
+    card: "🃞",
+  },
+  {
+    name: "DiamondsA",
+    suits: "Diamond",
+    value: 1,
+    number: "A",
+    color: "Red",
+    face: false,
+    symbol: "♦️",
+    card: "🃁",
+  },
+  {
+    name: "Diamonds2",
+    suits: "Diamond",
+    value: 2,
+    number: "2",
+    color: "Red",
+    face: false,
+    symbol: "♦️",
+    card: "🃂",
+  },
+  {
+    name: "Diamonds3",
+    suits: "Diamond",
+    value: 3,
+    number: "3",
+    color: "Red",
+    face: false,
+    symbol: "♦️",
+    card: "🃃",
+  },
+  {
+    name: "Diamonds4",
+    suits: "Diamond",
+    value: 4,
+    number: "4",
+    color: "Red",
+    face: false,
+    symbol: "♦️",
+    card: "🃄",
+  },
+  {
+    name: "Diamonds5",
+    suits: "Diamond",
+    value: 5,
+    number: "5",
+    color: "Red",
+    face: false,
+    symbol: "♦️",
+    card: "🃅",
+  },
+  {
+    name: "Diamonds6",
+    suits: "Diamond",
+    value: 6,
+    number: "6",
+    color: "Red",
+    face: false,
+    symbol: "♦️",
+    card: "🃆",
+  },
+  {
+    name: "Diamonds7",
+    suits: "Diamond",
+    value: 7,
+    number: "7",
+    color: "Red",
+    face: false,
+    symbol: "♦️",
+    card: "🃇",
+  },
+  {
+    name: "Diamonds8",
+    suits: "Diamond",
+    value: 8,
+    number: "8",
+    color: "Red",
+    face: false,
+    symbol: "♦️",
+    card: "🃈",
+  },
+  {
+    name: "Diamonds9",
+    suits: "Diamond",
+    value: 9,
+    number: "9",
+    color: "Red",
+    face: false,
+    symbol: "♦️",
+    card: "🃉",
+  },
+  {
+    name: "Diamonds10",
+    suits: "Diamond",
+    value: 10,
+    number: "10",
+    color: "Red",
+    face: false,
+    symbol: "♦️",
+    card: "🃊",
+  },
+  {
+    name: "DiamondsJ",
+    suits: "Diamond",
+    value: 11,
+    number: "J",
+    color: "Red",
+    face: true,
+    symbol: "♦️",
+    card: "🃋",
+  },
+  {
+    name: "DiamondsQ",
+    suits: "Diamond",
+    value: 12,
+    number: "Q",
+    color: "Red",
+    face: true,
+    symbol: "♦️",
+    card: "🃍",
+  },
+  {
+    name: "DiamondsK",
+    suits: "Diamond",
+    value: 13,
+    number: "K",
+    color: "Red",
+    face: true,
+    symbol: "♦️",
+    card: "🃎",
+  },
+];
+
+const joker = {
+  name: "Joker",
+  suits: "Joker",
+  value: null,
+  number: "Joker",
+  color: null,
+  face: true,
+  symbol: null,
+  card: "🃟",
+};
+
+const deckWithJoker = deckStandard.concat(joker); // concat makes a new arrow.
+
+function shuffleDeck(array) {
+  const newArray = Array.from(array);
+  for (let i = newArray.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [newArray[i], newArray[j]] = [newArray[j], newArray[i]];
+  }
+  return newArray;
+}
+
+let cardCounter = 0;
+let verticalScore = 0;
+let horizontalScore = 1;
+let skippable = true;
+let skipSign = false;
+let widthTemp = 1;
+let widthMax = 0;
+const myHand = [];
+const myHandHTML = [];
+const discardPile = [];
+const drawDeck = Array.from(shuffleDeck(deckStandard));
+
+function skipCards() {
+  cardCounter++;
+  if (cardCounter < 53) {
+    discardPile.unshift(drawDeck[0]);
+    drawDeck.shift();
+    // checkVertical();
+    // myHandHTMLfunc();
+    // nextPlusMinus1Checker();
+  }
+}
+
+function getCards() {
+  skipSign = false;
+  cardCounter++;
+  if (cardCounter < 53) {
+    checkVertical();
+    myHandHTMLfunc();
+    // nextPlusMinus1Checker();
+    if (widthTemp >= widthMax) {
+      widthMax = widthTemp;
+  }
+    console.log("widthTemp: " + widthTemp);  
+    console.log("widthMax: " + widthMax);   
+  }
+}
+
+function myHandHTMLfunc() {
+  myHandHTML.length = 0;
+  // this process is done from zero all the time, so don't count anything inside this.
+  for (i = 0; i < myHand.length; i++) {
+    if (i === 0) {
+      if (myHand[i].value === 1 || myHand[i].value === 13) {
+        myHandHTML.unshift("<br>");
+        myHandHTML.unshift("⏬");
+      }
+    }
+    if (i >= 1) {
+      if (myHand[i].value === 1 || myHand[i].value === 13) {
+        myHandHTML.unshift("<br>");
+        myHandHTML.unshift("⏬");
+      } else if (myHand[i - 1].value === myHand[i].value) {
+        myHandHTML.unshift("<br>");
+        myHandHTML.unshift("⏬");
+      }
+    }
+    // if (myHand[i].value !== 1 || myHand[i].value !== 13) {
+    // }
+    myHandHTML.unshift(myHand[i].number);
+    myHandHTML.unshift(myHand[i].symbol);
+  }
+}
+
+function checkVertical() {
+  if (myHand.length === 0) {
+    myHand.unshift(drawDeck[0]);
+    drawDeck.shift();
+    verticalScore++;
+    widthTemp = 1;
+  } /* myHand.length > 0 */ else {
+    if (myHand[0].value === 1) {
+      myHand.unshift(drawDeck[0]);
+      drawDeck.shift();
+      verticalScore++;
+      widthTemp = 1;
+    } else if (myHand[0].value === 13) {
+      myHand.unshift(drawDeck[0]);
+      drawDeck.shift();
+      verticalScore++;
+      widthTemp = 1;
+    } else {
+      if (myHand[0].value === drawDeck[0].value) {
+        myHand.unshift(drawDeck[0]);
+        drawDeck.shift();
+        verticalScore++;
+        widthTemp = 1;
+      } else {
+        checkHorizontal();
+      }
+    }
+  }
+}
+
+function checkHorizontal() {
+  if (myHand.length === 1) {
+    if (drawDeck[0].value === myHand[0].value + 1) {
+      myHand.unshift(drawDeck[0]);
+      drawDeck.shift();
+      horizontalScore++;
+      widthTemp++;
+    } else if (drawDeck[0].value === myHand[0].value - 1) {
+      myHand.unshift(drawDeck[0]);
+      drawDeck.shift();
+      horizontalScore++;
+      widthTemp++;
+    } else {
+      discardPile.unshift(drawDeck[0]);
+      drawDeck.shift();
+    }
+  } /* myHand.length > 1 */ else {
+    if (drawDeck[0].value === 13 && myHand[0].value === 12) {
+      // ex 13 => 12 => 13
+      myHand.unshift(drawDeck[0]);
+      drawDeck.shift();
+      horizontalScore++;
+      widthTemp++;
+    } else if (drawDeck[0].value === 1 && myHand[0].value === 2) {
+      // ex 1 => 2 => 1
+      myHand.unshift(drawDeck[0]);
+      drawDeck.shift();
+      horizontalScore++;
+      widthTemp++;
+    } else if (drawDeck[0].value === myHand[0].value + 1) {
+      // ex ?? => 8 => 9
+      if (myHand[0].value === myHand[1].value + 1) {
+        // if 7 => 8 => 9 : OK
+        myHand.unshift(drawDeck[0]);
+        drawDeck.shift();
+        horizontalScore++;
+        widthTemp++;
+      } else if (myHand[0].value === myHand[1].value) {
+        // if 8 => 8 => 9  : OK
+        myHand.unshift(drawDeck[0]);
+        drawDeck.shift();
+        horizontalScore++;
+        widthTemp++;
+      } else if (myHand[0].value === myHand[1].value - 1) {
+        // if 9 => 8 => 9  : NG
+        discardPile.unshift(drawDeck[0]);
+        drawDeck.shift();
+      } else if (myHand[1].value === 1) {
+        // if 1 => 8 => 9  : OK
+        myHand.unshift(drawDeck[0]);
+        drawDeck.shift();
+        horizontalScore++;
+        widthTemp++;
+      } else if (myHand[1].value === 13) {
+        // if 13 => 8 => 9  : OK
+        myHand.unshift(drawDeck[0]);
+        drawDeck.shift();
+        horizontalScore++;
+        widthTemp++;
+      }
+    } else if (drawDeck[0].value === myHand[0].value - 1) {
+      // ex ?? => 8 => 7
+      if (myHand[0].value === myHand[1].value - 1) {
+        // ex 9 => 8 => 7  : OK
+        myHand.unshift(drawDeck[0]);
+        drawDeck.shift();
+        horizontalScore++;
+        widthTemp++;
+      } else if (myHand[0].value === myHand[1].value) {
+        // ex 8 => 8 => 7  : OK
+        myHand.unshift(drawDeck[0]);
+        drawDeck.shift();
+        horizontalScore++;
+        widthTemp++;
+      } else if (myHand[0].value === myHand[1].value + 1) {
+        // if 7 => 8 => 7  : NG
+        discardPile.unshift(drawDeck[0]);
+        drawDeck.shift();
+      } else if (myHand[1].value === 1) {
+        // ex 1 => 8 => 7  : OK
+        myHand.unshift(drawDeck[0]);
+        drawDeck.shift();
+        horizontalScore++;
+        widthTemp++;
+      } else if (myHand[1].value === 13) {
+        // ex 13 => 8 => 7  : OK
+        myHand.unshift(drawDeck[0]);
+        drawDeck.shift();
+        horizontalScore++;
+        widthTemp++;
+      } else {
+        discardPile.unshift(drawDeck[0]);
+        drawDeck.shift();
+      }
+    } else {
+      discardPile.unshift(drawDeck[0]);
+      drawDeck.shift();
+    }
+  }
+}
