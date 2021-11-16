74c2379ec1c335217dce88d44f5aba5a0be6641f
tagoso <shirouo4279@gmail.com>
2021-11-16 21:51:44 +0000

    updated app.js

--- app.js
+++ app.js
@@ -2,5 +2,6 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+  // res.sendFile(__dirname + "/index.html");
+  res.send("Hello world!");
 });
