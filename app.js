673d4c3ad413c2297a65538e5b3d1598cb90671d
tagoso <shirouo4279@gmail.com>
2021-11-17 00:16:23 +0200

    deploy via heroku

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const app = express();
 
-// use the express-static middleware
+// use the express-static middleware.
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-// start the server listening for requests
+// start the server listening for requests.
 app.listen(process.env.PORT || 3000, () => console.log("Server is running..."));
