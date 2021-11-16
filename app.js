08a6d8d180dd2ffa16ddebcfb8bc63f1092515c6
tagoso <shirouo4279@gmail.com>
2021-11-16 22:01:41 +0000

    start the server listening for requests

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const app = express();
 
+// use the express-static middleware
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   // res.sendFile(__dirname + "/index.html");
   res.send("Hello world!");
 });
+
+// start the server listening for requests
+app.listen(process.env.PORT || 3000, () => console.log("Server is running..."));
